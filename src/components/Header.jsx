7829233e67fd16c29498a555b99240c1214c6124
Terry9022 <karta1020376@gmail.com>
2024-06-04 18:09:03 -0400

    minor header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
           className={`inline-block p-4 rounded-t-lg ${
             location.pathname === "/salaries"
               ? "text-blue-600 bg-gray-100 active "
-              : "hover:text-gray-600 hover:bg-gray-50 "
+              : "hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
           }`}
         >
           Salaries among CS careers
@@ -33,7 +33,7 @@ function Header() {
           className={`inline-block p-4 rounded-t-lg ${
             location.pathname === "/competitiveness"
               ? "text-blue-600 bg-gray-100 active "
-              : "hover:text-gray-600 hover:bg-gray-50 "
+              : "hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
           }`}
         >
           Competitiveness of entry-level jobs
@@ -45,7 +45,7 @@ function Header() {
           className={`inline-block p-4 rounded-t-lg ${
             location.pathname === "/influence-genai"
               ? "text-blue-600 bg-gray-100 active "
-              : "hover:text-gray-600 hover:bg-gray-50 "
+              : "hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
           }`}
         >
           Influence of GenAI on job opportunities
@@ -57,7 +57,7 @@ function Header() {
           className={`inline-block p-4 rounded-t-lg ${
             location.pathname === "/job-postings"
               ? "text-blue-600 bg-gray-100 active "
-              : "hover:text-gray-600 hover:bg-gray-50 "
+              : "hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
           }`}
         >
           Job postings in the CS field
