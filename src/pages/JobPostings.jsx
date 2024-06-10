bb6c90cfeefc027db110c28b01ec6cc124131bb2
Terry9022 <karta1020376@gmail.com>
2024-06-10 16:55:07 -0400

    minor

--- src/pages/JobPostings.jsx
+++ src/pages/JobPostings.jsx
@@ -10,7 +10,7 @@ const JobPostings = () => {
       <div className="p-4">
         <div className="flex-col justify-between items-center">
           <h1 className="text-3xl mb-2 text-center text-red-800 font-extrabold cursor-default">
-            COMM-6880 Interactive Data Visualization
+            Interactive Data Visualization
           </h1>
           <Header />
         </div>
--- src/pages/Salaries.jsx
+++ src/pages/Salaries.jsx
@@ -10,15 +10,15 @@ const Salaries = () => {
       <div className="p-4">
         <div className="flex-col justify-between items-center">
           <h1 className="text-3xl mb-2 text-center text-red-800 font-extrabold cursor-default">
-            COMM-6880 Interactive Data Visualization
+            Interactive Data Visualization
           </h1>
           <Header />
         </div>
       </div>
       <div style={{ width: "60%", height: "600px", margin: "auto" }}>
-        <SalariesBarChart/>
+        <SalariesBarChart />
         <div className="m-[30px]"></div>
-        <SalariesScatterChart/>
+        <SalariesScatterChart />
       </div>
     </div>
   );
